d14fc844408a6c36422eae6ffdffe609e9fb47bd
julioceaseless <julioceaseless@gmail.com>
2023-10-31 15:20:03 +0000

    fix print statement for zero

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,7 +9,7 @@ if number < 0:
 if result > 5:
     print(f"Last digit of {number:d} is {result:d} and is greater than {5:d}")
 elif result == 0:
-    print(f"Last digit of {number:d} is {result:d} and {0:d}")
+    print(f"Last digit of {number:d} is {result:d} and is {0:d}")
 else:
     print(f"Last digit of {number:d} is {result:d} and is less" +
           " than 6 and not 0")
