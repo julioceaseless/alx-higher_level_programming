9fc12e3c51ccce6640ecb4ccb207ce7d9b08a4ce
julioceaseless <julioceaseless@gmail.com>
2023-11-01 06:26:37 +0000

    different approach

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-
-
 def remove_char_at(str, n):
     result = ""
-    count = 0
+    count = -1
     for letter in str:
+        count = count + 1
         if count == n:
             count += 1
             continue
         result += letter
-        count += 1
     return result
