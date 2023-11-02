c313bfe1ced57d7be3ecc620dd02cddefcd80983
julioceaseless <julioceaseless@gmail.com>
2023-11-02 13:08:18 +0000

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-if len(argv) == 1:
-    print("0 arguments.")
-elif len(argv) == 2:
-    print("1 argument:")
-    print("{:d}: {}".format(1, argv[1]))
-else:
-    print("{:d} arguments:".format(len(argv) - 1))
-    for i in range(1, len(argv)):
-        print("{:d}: {}".format(i, argv[i]))
+    if len(argv) == 1:
+        print("0 arguments.")
+    elif len(argv) == 2:
+        print("1 argument:")
+        print("{:d}: {}".format(1, argv[1]))
+    else:
+        print("{:d} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{:d}: {}".format(i, argv[i]))
