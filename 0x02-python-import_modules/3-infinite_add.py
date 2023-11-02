0f5e74597457e12cbab752643e0bbde8b48a78f1
julioceaseless <julioceaseless@gmail.com>
2023-11-02 13:22:00 +0000

    fix indentation for while loop

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,12 +5,9 @@ if __name__ == "__main__":
     result = 0
     if len(argv) == 1:
         print("{:d}".format(result))
-    elif len(argv) == 2:
-        result = int(argv[1])
-        print("{:d}".format(result))
     else:
         i = 1
         while i < len(argv):
             result += int(argv[i])
             i = i + 1
-            print("{:d}".format(result))
+        print("{:d}".format(result))
