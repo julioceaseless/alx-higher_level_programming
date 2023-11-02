e8d90a0ba774b910a2bb0500b4a8e84cb94be308
julioceaseless <julioceaseless@gmail.com>
2023-11-02 13:11:39 +0000

    quick fix

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-result = 0
-if len(argv) == 1:
-    print("{:d}".format(result))
-elif len(argv) == 2:
-    result = int(argv[1])
-    print("{:d}".format(result))
-else:
-    i = 1
-    while i < len(argv):
-        result += int(argv[i])
-        i = i + 1
-    print("{:d}".format(result))
+    result = 0
+    if len(argv) == 1:
+        print("{:d}".format(result))
+    elif len(argv) == 2:
+        result = int(argv[1])
+        print("{:d}".format(result))
+    else:
+        i = 1
+        while i < len(argv):
+            result += int(argv[i])
+            i = i + 1
+            print("{:d}".format(result))
