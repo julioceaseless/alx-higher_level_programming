56fa4b7d8bc4a20ed71cc190337f4a55ab120d6f
julioceaseless <julioceaseless@gmail.com>
2023-11-02 13:28:37 +0000

    prevent code from executing when imported

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from sys import argv, exit
-from calculator_1 import add, div, mul, sub
 if __name__ == "__main__":
+    from sys import argv, exit
+    from calculator_1 import add, div, mul, sub
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
