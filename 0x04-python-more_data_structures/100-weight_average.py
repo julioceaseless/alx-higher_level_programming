241d69962a4b865eabb74c2e56268d0d3f22a2c5
julioceaseless <julioceaseless@gmail.com>
2023-11-10 15:50:24 +0000

    different approach

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is None:
-        return 0
-    divisor = 0
-    dividend = 0
-    quotient = 0
-
-    list_len = len(my_list)
-    for i in range(list_len):
-        dividend += my_list[i][0] * my_list[i][1]
-        divisor += my_list[i][1]
-    quotient = dividend / divisor
-    return quotient
+    if my_list:
+        dividend = sum(s * w for s, w in my_list)
+        divisor = sum(w for s, w in my_list)
+        return (dividend/divisor)
+    return (0)
