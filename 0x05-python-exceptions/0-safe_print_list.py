7ada2b98710cf3caf67fdf91562d0b2bd3726c18
julioceaseless <julioceaseless@gmail.com>
2023-11-20 15:32:35 +0000

    pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     try:
         while (items < x):
             print("{}".format(my_list[items]), end="")
-            items = items + 1;
-        print();
-    except:
+            items = items + 1
+        print()
+    except Exception:
         print()
     return (items)
