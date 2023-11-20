aad0adfc65e702319aadfb1fdf529d5cec743ac5
julioceaseless <julioceaseless@gmail.com>
2023-11-20 17:32:56 +0000

    safe print integer error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        return True
     except ValueError as ve:
         sys.stderr.write("Exception: " + str(ve) + '\n')
         return False
+    return True
