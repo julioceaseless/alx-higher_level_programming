d1ef2dbe3c6d58c316445a534df65c38e8c76b46
julioceaseless <julioceaseless@gmail.com>
2023-11-20 17:16:10 +0000

    fix errors

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -15,6 +15,7 @@ def list_division(my_list_1, my_list_2, list_length):
             continue
         except IndexError:
             print("out of range")
+            result = 0
             continue
         finally:
             result_list.append(result)
