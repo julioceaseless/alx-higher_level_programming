c44962bee900089791f3ba4d8ed0d6626a51577b
julioceaseless <julioceaseless@gmail.com>
2023-11-22 02:58:52 +0000

    change

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,12 +9,8 @@ size(private instance attribute)
 class Square:
     """Initialize the class"""
     def __init__(self, size=0):
-        if (type(size) != int):
+        if (type(size) is not int):
             raise TypeError("size must be an integer")
         elif (size < 0):
             raise ValueError("size must be >=0")
         self.__size = size
-
-    """Define the square"""
-    def area(self):
-        return self.__size * __size
