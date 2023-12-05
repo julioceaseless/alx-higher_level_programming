99ab76a5d61ddccf5bdf59d6795ab660dc71bfc6
julioceaseless <julioceaseless@gmail.com>
2023-12-05 02:52:33 +0000

    create rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
-    
+
     def area(self):
         """
         Implement area for rectangle
