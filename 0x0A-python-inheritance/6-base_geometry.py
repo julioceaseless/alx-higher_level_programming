8648fc507c5a0ef2c2129a890cbb6619be055b92
julioceaseless <julioceaseless@gmail.com>
2023-12-05 01:55:32 +0000

    base area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Creates an empty class
+"""
+
+
+class BaseGeometry:
+    """
+    this is an empty class
+    """
+
+    def area(self):
+        """
+        area is not implemented
+        """
+
+        raise Exception("area() is not implemented")
