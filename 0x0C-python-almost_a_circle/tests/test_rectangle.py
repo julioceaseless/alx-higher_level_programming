934eb02c7641323d2ca3e0e5794bb258fb3d13d0
julioceaseless <julioceaseless@gmail.com>
2023-12-14 07:45:29 +0000

    more unit tests

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -15,28 +15,102 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(rect.width, 3)
         self.assertEqual(rect.height, 4)
 
-    def test_negative_values(self):
-        # Test if the class handles negative values correctly
+    """ Test negative values """
+    def test_negative_width_value(self):
+        # Test if the class handles negative width values correctly
         with self.assertRaises(ValueError) as err:
             rect = Rectangle(-3, 4)
         msg = "width must be > 0"
         self.assertEqual(str(err.exception), msg)
 
-    def test_negative_values(self):
-        # Test if the class handles negative values correctly
+    def test_negative_height_value(self):
+        # Test if the class handles negative height values correctly
         with self.assertRaises(ValueError) as err:
             rect = Rectangle(3, -4)
         msg = "height must be > 0"
         self.assertEqual(str(err.exception), msg)
 
-    def test_zero(self):
+    def test_negative_x_value(self):
+        # Test if the class handles negative x value correctly
+        with self.assertRaises(ValueError) as err:
+            rect = Rectangle(3, 4, -4)
+        msg = "x must be >= 0"
+        self.assertEqual(str(err.exception), msg)
+
+    def test_negative_y_value(self):
+        # Test if the class handles negative y value correctly
+        with self.assertRaises(ValueError) as err:
+            rect = Rectangle(3, 4, 2, -3)
+        msg = "y must be >= 0"
+        self.assertEqual(str(err.exception), msg)
+
+    """ Test string values """
+    def test_str_width_value(self):
+        # Test if the class handles non-integer values correctly
+        with self.assertRaises(TypeError) as err:
+            rect = Rectangle("three", 10)
+        msg = "width must be an integer"
+        self.assertEqual(str(err.exception), msg)
+
+    def test_str_height_value(self):
+        # Test if the class handles non-integer values correctly
+        with self.assertRaises(TypeError) as err:
+            rect = Rectangle(3, "10")
+        msg = "height must be an integer"
+        self.assertEqual(str(err.exception), msg)
+
+    def test_str_x_value(self):
+        # Test if the class handles string x values correctly
+        with self.assertRaises(TypeError) as err:
+            rect = Rectangle(3, 10, "one")
+        msg = "x must be an integer"
+        self.assertEqual(str(err.exception), msg)
+
+    def test_str_y_value(self):
+        # Test if the class handles string y values correctly
+        with self.assertRaises(TypeError) as err:
+            rect = Rectangle(3, 10, 1, "one")
+        msg = "y must be an integer"
+        self.assertEqual(str(err.exception), msg)
+
+    """ Test float values"""
+    def test_float_x_value(self):
+        # Test if the class handles float  x values correctly
+        with self.assertRaises(TypeError) as err:
+            rect = Rectangle(3, 10, 1.1, 1)
+        msg = "x must be an integer"
+        self.assertEqual(str(err.exception), msg)
+
+    def test_float_y_value(self):
+        # Test if the class handles float y values correctly
+        with self.assertRaises(TypeError) as err:
+            rect = Rectangle(3, 10, 1, 2.1)
+        msg = "y must be an integer"
+        self.assertEqual(str(err.exception), msg)
+
+    def test_float_height_value(self):
+        # Test if the class handles float height value correctly
+        with self.assertRaises(TypeError) as err:
+            rect = Rectangle(3, 5.5)
+        msg = "height must be an integer"
+        self.assertEqual(str(err.exception), msg)
+
+    def test_float_width_value(self):
+        # Test if the class handles float width value correctly
+        with self.assertRaises(TypeError) as err:
+            rect = Rectangle(3.5, 10)
+        msg = "width must be an integer"
+        self.assertEqual(str(err.exception), msg)
+
+    """ test zero values """
+    def test_zero_width(self):
         # Test if the class handles negative values correctly
         with self.assertRaises(ValueError) as err:
             rect = Rectangle(0, 4)
         msg = "width must be > 0"
         self.assertEqual(str(err.exception), msg)
 
-    def test_zero(self):
+    def test_zero_height(self):
         # Test if the class handles negative values correctly
         with self.assertRaises(ValueError) as err:
             rect = Rectangle(4, 0)
--- 0x0C-python-almost_a_circle/tests/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -15,20 +15,73 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(sqr.width, 3)
         self.assertEqual(sqr.height, 3)
 
-    def test_negative_values(self):
-        # Test if the class handles negative values correctly
-        with self.assertRaises(ValueError) as err:
-            sqr = Square(-3)
-        msg = "width must be > 0"
+    """ Test value types """
+    def test_str_size_value(self):
+        # Test if the class handles string size values correctly
+        with self.assertRaises(TypeError) as err:
+            sqr = Square("10")
+        msg = "width must be an integer"
         self.assertEqual(str(err.exception), msg)
 
-    def test_negative_values(self):
+    def test_str_x_value(self):
+        # Test if the class handles string x values correctly
+        with self.assertRaises(TypeError) as err:
+            sqr = Square(1, 2, "10")
+        msg = "y must be an integer"
+        self.assertEqual(str(err.exception), msg)
+
+    def test_str_y_value(self):
+        # Test if the class handles string y values correctly
+        with self.assertRaises(TypeError) as err:
+            sqr = Square(2, 4, "10")
+        msg = "y must be an integer"
+        self.assertEqual(str(err.exception), msg)
+
+    """ Test float values """
+    def test_float_value(self):
+        # Test if the class handles float values correctly
+        with self.assertRaises(TypeError) as err:
+            sqr = Square(3.14)
+        msg = "width must be an integer"
+        self.assertEqual(str(err.exception), msg)
+
+    def test_float_x_value(self):
+        # Test if the class handles float  x values correctly
+        with self.assertRaises(TypeError) as err:
+            sqr = Square(3, 1.1, 1)
+        msg = "x must be an integer"
+        self.assertEqual(str(err.exception), msg)
+
+    def test_float_y_value(self):
+        # Test if the class handles float y values correctly
+        with self.assertRaises(TypeError) as err:
+            sqr = Square(3, 1, 2.1)
+        msg = "y must be an integer"
+        self.assertEqual(str(err.exception), msg)
+
+    """ Test negative values """
+    def test_negative_size_value(self):
         # Test if the class handles negative values correctly
         with self.assertRaises(ValueError) as err:
             sqr = Square(-4)
         msg = "width must be > 0"
         self.assertEqual(str(err.exception), msg)
 
+    def test_negative_x_value(self):
+        # Test if the class handles negative x values correctly
+        with self.assertRaises(ValueError) as err:
+            sqr = Square(3, -2, 1)
+        msg = "x must be >= 0"
+        self.assertEqual(str(err.exception), msg)
+
+    def test_negative_y_value(self):
+        # Test if the class handles negative y values correctly
+        with self.assertRaises(ValueError) as err:
+            sqr = Square(3, 2, -1)
+        msg = "y must be >= 0"
+        self.assertEqual(str(err.exception), msg)
+
+    """ Test zero size values """
     def test_zero(self):
         # Test if the class handles negative values correctly
         with self.assertRaises(ValueError) as err:
