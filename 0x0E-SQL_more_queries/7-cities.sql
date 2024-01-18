d053fee09dbe1074fe075becac25b08a9f34d958
julioceaseless <julioceaseless@gmail.com>
2024-01-18 08:28:51 +0000

    set pk

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,9 @@
 -- create table cities
 
 CREATE TABLE cities (
-	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	id INT AUTO_INCREMENT NOT NULL,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id),
 	FOREIGN KEY (state_id) REFERENCES states(id)
 );
