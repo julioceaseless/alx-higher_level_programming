f1f5141a23bb1ad500919e7010da27f14d4900c6
julioceaseless <julioceaseless@gmail.com>
2024-01-18 09:33:21 +0000

    remove aliases

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- display count of grouped entries
 
-SELECT tg.name as genre, COUNT(tsg.genre_id) AS number_of_shows 
-FROM tv_genres tg 
-JOIN tv_show_genres tsg ON tsg.genre_id = tg.id 
-GROUP BY tg.name 
+SELECT tv_genres.name as genre, COUNT(tv_show_genres.genre_id) AS number_of_shows 
+FROM tv_genres
+JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id 
+GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
