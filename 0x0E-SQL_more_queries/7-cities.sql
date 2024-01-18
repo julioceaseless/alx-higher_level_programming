28a680d850ee5b8071818344d66026efdf1f4eca
julioceaseless <julioceaseless@gmail.com>
2024-01-18 08:34:03 +0000

    set pk

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,12 @@
 -- create table cities
 
+--create database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- use database
+USE hbtn_0d_usa;
+
+-- create table
 CREATE TABLE IF NOT EXISTS cities (
 	id INT AUTO_INCREMENT NOT NULL,
 	state_id INT NOT NULL,
