038c55d572172a999ca6474211f5cde6981b25ef
julioceaseless <julioceaseless@gmail.com>
2024-01-18 02:58:18 +0000

    more sql

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- List all cities with their corresponding state names using JOIN
+SELECT cities.id, cities.name AS city_name, states.name AS state_name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
