2e064383660987a6a5a6e6ffc7e884ebc3025cf0
julioceaseless <julioceaseless@gmail.com>
2024-01-19 00:50:14 +0000

    correction

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,6 +2,6 @@
 
 SELECT tv_genres.name as genre, COUNT(tv_show_genres.genre_id) AS number_of_shows 
 FROM tv_genres
-JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id 
+INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id 
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
