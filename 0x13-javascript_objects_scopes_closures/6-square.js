087c8c3ce1e63ca54490f8f3d2552f2bbed5ea3f
julioceaseless <julioceaseless@gmail.com>
2024-02-14 02:24:16 +0000

    fix logic

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const Rectangle = require('./5-square');
 
 class Square extends Rectangle {
   constructor (size) {
@@ -8,16 +8,15 @@ class Square extends Rectangle {
   }
 
   charPrint (c) {
-    if (c) {
-      for (let h = 0; h < this.width; h++) {
-        let line = '';
-        for (let w = 0; w < this.width; w++) {
-          line += c;
-        }
-        console.log(line);
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let h = 0; h < this.width; h++) {
+      let line = '';
+      for (let w = 0; w < this.width; w++) {
+        line += c;
       }
-    } else {
-      super.print();
+      console.log(line);
     }
   }
 }
