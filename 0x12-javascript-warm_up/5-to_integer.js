b0c7c53b6c0cebc3471f76078227feca9186a19b
julioceaseless <julioceaseless@gmail.com>
2024-02-15 02:07:16 +0000

    added message and fixed truthy logic

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const num = parseInt(process.argv[2]);
-if (num) {
-  console.log(num);
-} else {
+if (num === undefined) {
   console.log('Not a number');
+} else {
+  console.log('My number: ' + num);
 }
