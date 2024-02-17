e19e2ae25dffafb415c5995dea7bd25544dd0fdf
julioceaseless <julioceaseless@gmail.com>
2024-02-17 21:04:08 +0000

    fix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 import sys
+"""This module allows access to command line arguments"""
 import MySQLdb
-"""Query MYSQL database via python"""
+"""This modules allows connection to MySQL database"""
 
 
-if __name__ == "__main__":
-    username, password, name = sys.argv[1:]
+def main(username, password, db_name):
+    """Queries the database
+
+    Args:
+        username: database username
+        password: user's password
+        db_name: database name
+    """
+
     conn = MySQLdb.connect(host="localhost", port=3306,
                            user=username, passwd=password,
-                           db=name, charset="utf8")
+                           db=db_name, charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
     query_rows = cur.fetchall()
@@ -16,3 +24,9 @@ if __name__ == "__main__":
         print(row)
     cur.close()
     conn.close()
+
+
+if __name__ == "__main__":
+    """don't run when imported"""
+
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
