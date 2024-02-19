89139642ab3aa8af5e0536966c6015cdc0e833c7
julioceaseless <julioceaseless@gmail.com>
2024-02-19 20:05:23 +0000

    fetch first entry

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+script lists all State objects from database using sqlalchemy
+"""
+
+
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+from model_state import State, Base
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(engine)
+    session = Session()
+
+    state = session.query(State).filter(State.id == 1).one_or_none()
+    print(f'{state.id}: {state.name}')
