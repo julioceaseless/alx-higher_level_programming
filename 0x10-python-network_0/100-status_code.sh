0af5c88d6ef56e56a646733a844a8d233dc20111
julioceaseless <julioceaseless@gmail.com>
2024-03-01 00:39:26 +0000

    correction

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# script sends a request to URL passed as arguent and displays the status code of response
+# displays the status code of response
 curl -s -o /dev/null -w "%{http_code}" 1$
 
