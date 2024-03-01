75930d281f6d511a127c96edeb1e25c3e04c1613
julioceaseless <julioceaseless@gmail.com>
2024-03-01 02:44:29 +0000

    hack

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!
-curl -sL -X PUT -H "Origin: School" --data "user_id=98" $1
+curl -s -X PUT -H "Origin: School" --data "user_id=98" $1_3
