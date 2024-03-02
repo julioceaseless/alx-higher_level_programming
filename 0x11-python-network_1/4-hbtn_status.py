caf5b57e58a050f1d80f37de090c5edc69c14cd7
julioceaseless <julioceaseless@gmail.com>
2024-03-02 02:55:18 +0000

    get status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+script uses requests module to fetch https://intranet.hbtn.io/status
+"""
+import requests
+
+
+if __name__ == "__main__":
+    """execute script only when called directly"""
+    res = requests.get("https://intranet.hbtn.io/status")
+    body = res.text
+    print("Body response:")
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
