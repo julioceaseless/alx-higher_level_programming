035311bb245b64bf30486d80ccc78c78246e0443
julioceaseless <julioceaseless@gmail.com>
2024-03-02 01:42:12 +0000

    fetch from url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This script feches https://intranet.hbtn.io/status using urllib module
+"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    """ only run when called directly"""
+
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+        body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
