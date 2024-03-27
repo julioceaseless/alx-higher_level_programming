b15507d2c8735c7ae1b5efd061e496d0dfe3a2e4
julioceaseless <julioceaseless@gmail.com>
2024-03-27 03:01:38 +0000

    code optimization

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,17 +2,12 @@
 
 const request = require('request');
 
-const query = parseInt(process.argv[2]);
-const url = 'https://swapi-api.alx-tools.com/api/films/';
+const movieId = parseInt(process.argv[2]);
+const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
 request(url, { json: true }, function (err, response, body) {
   if (err) {
     throw (err);
   }
-  for (let i = 0; i < body.results.length; i++) {
-    if (body.results[i].episode_id === query) {
-      console.log(body.results[i].title);
-      break;
-    }
-  }
+  console.log(body.title);
 });
